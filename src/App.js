b9d2241991b140aba8052cd2cd53966bebc6afa8
Felipe <f.zende@gmail.com>
2021-02-01 18:10:36 +0100

    no hagas push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Footer from "./components/footer/Footer";
 import Header from "./components/Header/Header";
@@ -26,3 +25,23 @@ function App() {
 }
 
 export default App;
+
+
+/* function App() {
+  return (
+    <div className="App">
+      <>
+       <Header/>
+      <IntroducePage/>
+      <TrustBox/>
+      <div className="secondprincipal__container">
+      <BoxInfo/>
+      <img src={SecondPicture} alt="sexy lady" className="sexyLady"/>
+      </div>
+      <Footer/>
+      </>
+    </div>
+  );
+}
+
+export default App; */
